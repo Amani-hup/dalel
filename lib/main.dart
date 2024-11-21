87613d56540c27be61a8a97c4c063404ccef5b61
amani-hup <Amani_tt168tt@icloud.com>
2024-11-21 21:38:32 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class Dalele extends StatelessWidget {
+  const Dalele({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
